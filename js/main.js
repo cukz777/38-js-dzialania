8b9abb5cb75d420c6187eabd18e031e7eeedb9ab
marcin <brand.marcin@gmail.com>
2023-02-07 16:45:24 +0000

    zadanie43675

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function calculate(num1, num2) {
+    let sum = num1 + num2;
+    let difference = num1 - num2;
+    let multi = num1 * num2;
+
+    if (sum >= 0) {
+        console.log(`Wynik dodawania wynosi ${sum}`);
+    } else {
+        console.log(`Wynik jest nieprawidłowy`);
+    }
+
+    if (difference >= 0) {
+        console.log(`Wynik odejmowania wynosi ${difference}`);
+    } else {
+        console.log(`Wynik jest nieprawidłowy`);
+    }
+
+    if (multi >= 0) {
+        console.log(`Wynik mnożenia wynosi ${product}`);
+    } else {
+        console.log(`Wynik jest nieprawidłowy`);
+    }
+}
